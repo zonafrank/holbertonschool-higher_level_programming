3d988806bdeaac53476efb8ef75da62f36ce2bd6
Eche <echyth@yahoo.com>
2024-01-23 21:54:46 +1000

    feature: task 8 - initial commit

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    res = ""
+    for char in str:
+        ascii_val = ord(char)
+        if ascii_val >= 97 and ascii_val <= 122:
+            res += chr(ascii_val - 32)
+        else:
+            res += char
+
+    return res
