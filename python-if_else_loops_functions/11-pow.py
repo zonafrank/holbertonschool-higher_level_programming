ee69fa6feaca868a9d3292121915e9ecaf8aba06
Eche <echyth@yahoo.com>
2024-01-23 22:37:28 +1000

    fix: task 11 - update pow function to handle negative power

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,16 @@
 
 def pow(a, b):
     res = 1
-    for i in range(b):
-        res *= a
+    sign = -1 if b < 0 else 1
+    for i in range(abs(b)):
+        if sign < 0:
+            res /= a
+        else:
+             res *= a
     return res
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
