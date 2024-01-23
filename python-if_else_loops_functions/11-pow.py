198c01f06231116a9749d180f4aaeb9f6d47d1d8
Eche <echyth@yahoo.com>
2024-01-23 22:41:25 +1000

    fix: task 11 - simplify function to use builting operator

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    res = 1
-    sign = -1 if b < 0 else 1
-    for i in range(abs(b)):
-        if sign < 0:
-            res /= a
-        else:
-             res *= a
-    return res
+    return a ** b
