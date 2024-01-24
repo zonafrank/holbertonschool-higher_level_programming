03e905d892c9f04755e1f122e650e1e329c37304
Eche <echyth@yahoo.com>
2024-01-24 23:20:21 +1000

    fix: task 9 - handle list of negative integers

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_val = 0
+    max_val = my_list[0]
     for val in my_list:
         if val > max_val:
             max_val = val
