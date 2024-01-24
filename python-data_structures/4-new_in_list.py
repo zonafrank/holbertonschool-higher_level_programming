05cbb5418c9bfc1bc4ec61d725ba328b04ab19ba
Eche <echyth@yahoo.com>
2024-01-24 21:55:20 +1000

    fix: task 4 - coding style

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,6 +4,5 @@ def new_in_list(my_list, idx, element):
     list_copy = [val for val in my_list]
     if idx < 0 or idx >= len(list_copy):
         return list_copy
-    
     list_copy[idx] = element
     return list_copy
