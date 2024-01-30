5caad73131c6d7cae7cd4e1f5b154da48d240ec6
Echezona Oji <echyth@yahoo.com>
2024-01-30 22:45:56 +1000

    refactor: task 6 - modify my_print method

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -38,9 +38,9 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             for j in range(self.__size):
-                for k in range(self.__position[0]):
+                for _ in range(0, self.__position[0]):
                     print(" ", end="")
-                for l in range(self.__size):
+                for _ in range(0, self.__size):
                     print("#", end="")
                 print()
 
