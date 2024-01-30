84c088b0a05dd77e49370d892712647793010eda
Echezona Oji <echyth@yahoo.com>
2024-01-30 12:06:05 +1000

    refactor: task 0 - update code to use try/except/finally

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,5 +8,6 @@ def safe_print_list(my_list=[], x=0):
             count += 1
     except IndexError as e:
         ...
-    print("".format(""))
-    return count
+    finally:
+        print("".format(""))
+        return count
