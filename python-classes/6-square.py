a8e986ea834c44db79e7986c0c4c999b36fa59f4
Echezona Oji <echyth@yahoo.com>
2024-01-30 21:42:57 +1000

    feature: task 6 - initial commit

---
+++ python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Program that creates a Square class"""
+
+
+class Square:
+    """ initializes the instance of the class"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """ Returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ getter method for size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ setter method for size property """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        """
+        that prints in stdout the square with the character #
+        prints a new line of size is zero
+        """
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print("{}{}".format("_" * self.position[0], "#" * self.size))
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (not isinstance(value[0], int)) or (not isinstance(value[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
