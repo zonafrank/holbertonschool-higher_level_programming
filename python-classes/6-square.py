a5cd36d10dd785fa3dad8d654b203a6c1bec0f91
Echezona Oji <echyth@yahoo.com>
2024-01-30 23:58:25 +1000

    refactor: task 6 - add more conditional checks in position method

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -73,6 +73,10 @@ class Square:
         """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if (not isinstance(value[0], int)) or (not isinstance(value[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
