365101fdbc91da7912c49896c4bb4d3a4ed41a62
Echezona Oji <echyth@yahoo.com>
2024-01-30 18:57:50 +1000

    doc: task 1 - modify documentation spacing

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""program that creates the class Square defined by its size"""
+
+
 class Square:
-    """Defines a square"""
+    """size is a private attribute of the class Square which is an integer"""
     def __init__(self, size):
-        """Initialises the data"""
         self.__size = size
