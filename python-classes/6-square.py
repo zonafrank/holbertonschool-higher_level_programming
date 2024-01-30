8b9061c5c7760bc8e687a9b2f52dc1f9d99f5e43
Echezona Oji <echyth@yahoo.com>
2024-01-30 22:25:53 +1000

    refactor: task 6 - modify my_print method

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,10 +35,11 @@ class Square:
         if self.size == 0:
             print()
         else:
-            for i in range(self.position[1]):
-                print()
-            for i in range(self.size):
-                print("{}{}".format(" " * self.position[0], "#" * self.size))
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
 
     @property
     def position(self):
