4951aba2896d5c2ee87b2a2c6a2436b03ae94eea
Echezona Oji <echyth@yahoo.com>
2024-01-30 16:14:25 +1000

    fix: task 4 - modify code to use string.format

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,19 +2,21 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    for i in range(list_length):
-        try:
-            a = my_list_1[i]
-            b = my_list_2[i]
-            result = a / b
-            result_list.append(result)
-        except ZeroDivisionError:
-            print("division by 0")
-            result_list.append(0)
-        except TypeError:
-            print("wrong type")
-            result_list.append(0)
-        except IndexError:
-            print("out of range")
-            result_list.append(0)
-    return result_list
+    try:
+        for i in range(list_length):
+            try:
+                a = my_list_1[i]
+                b = my_list_2[i]
+                result_list.append(a / b)
+            except ZeroDivisionError:
+                print("{}".format("division by 0"))
+                result_list.append(0)
+            except TypeError:
+                print("{}".format("wrong type"))
+                result_list.append(0)
+            except IndexError:
+                print("{}".format("out of range"))
+                result_list.append(0)
+                break
+    finally:
+        return result_list
