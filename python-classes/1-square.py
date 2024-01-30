8991f7eafe8817a5a0013b9ca5e8629e6e3d72f2
Echezona Oji <echyth@yahoo.com>
2024-01-30 18:45:23 +1000

    doc: task 1 - add documentation for the class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,8 +5,5 @@ class Square:
     def __init__(self, size):
         """
         Initializes a square with a given size.
-
-        Parameters:
-        - size: Size of the square (no type or value verification)
         """
         self.__size = size
