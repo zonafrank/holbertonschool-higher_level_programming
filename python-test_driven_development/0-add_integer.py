9f43842353fce01be5e48c9bc9dc40339bf7a663
Echezona Oji <echyth@yahoo.com>
2024-01-31 22:31:21 +1000

    feature: task 0 - initial commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Task 0. Integers addition"""
+
+
+def add_integer(a, b=98):
+    """Function that adds two integers
+
+    Args:
+        a (int): The first integer
+        b (int): The second integer
+
+    Returns:
+        int: the sum of a and b
+
+    Raises:
+        TypeError: Either of a and b is not an integer
+    """
+    try:
+        a = int(a)
+    except Exception as e:
+        raise TypeError("a must be an integer")
+    
+    try:
+        b = int(b)
+    except Exception as e:
+        raise TypeError("b must be an integer")
+ 
+    return a + b
