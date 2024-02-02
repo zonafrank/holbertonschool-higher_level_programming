76e5b7f46eb3f70d92ca79771091bd3f761d15c0
Echezona Oji <echyth@yahoo.com>
2024-02-02 12:04:16 +1000

    doc: task 0 - add module and function documentation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+""" This module contains one function to print a square"""
+
 
 def print_square(size):
+    """
+    prints a square of '#' characters
+
+    Args:
+        size (int): size of the square, length and breadth
+
+    Returns:
+        Nothing
+    """
     if any([isinstance(size, t) for t in [int, float]]):
         if isinstance(size, float) and size < 0:
             raise TypeError("size must be an integer")
