22f6b85e0e94b240eb93c9ad3a873c8f1b94bac9
Echezona Oji <echyth@yahoo.com>
2024-02-05 15:57:35 +1000

    fix: task 1 - update __init__ to use setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Rectangle class"""
     def __init__(self, width=0, height=0):
         """initializes an instance of class with data"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
