cbd60521efb64a04758be4e78fe4322cc09f91d1
Echezona Oji <echyth@yahoo.com>
2024-02-05 15:50:50 +1000

    style: task 1 - remove whitespaces in empty lines

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,13 +13,13 @@ class Rectangle:
     def width(self):
         """returns the width of the rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """changes the width of a rectangle instance"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        
+
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -28,13 +28,13 @@ class Rectangle:
     def height(self):
         """returns the height of the rectangle"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """changes the height of a rectangle instance"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        
+
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
