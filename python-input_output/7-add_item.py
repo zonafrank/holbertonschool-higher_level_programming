88befe50257e138213d9d83be0d23f2b7707b7c9
Echezona Oji <echyth@yahoo.com>
2024-02-08 00:18:31 +1000

    feature(task 7): implement solution in file 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    obj = []
+
+for i in range(1, len(sys.argv)):
+    obj.append(sys.argv[i])
+
+save_to_json_file(obj, "add_item.json")
