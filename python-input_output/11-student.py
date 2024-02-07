7a934cc61eddc3e628ae7567ead4e1c69ff1f939
Echezona Oji <echyth@yahoo.com>
2024-02-08 00:14:06 +1000

    feature(task 11): implement method reload_from_json in class Student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+        result = {}
+        for key in self.__dict__:
+            if key in attrs:
+                result[key] = self.__dict__[key]
+        return result
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.__dict__[key] = json[key]
