1e39b359d72466bc692fb28c43a77b08528622ba
Echezona Oji <echyth@yahoo.com>
2024-02-08 09:51:24 +1000

    feature(task 1): implement class MyList with print_sorted method

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """MyList class that inherits from list type"""
+
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
