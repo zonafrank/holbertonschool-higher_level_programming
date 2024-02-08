5831b80e972d6c6f7de96696576850a38b4e02ba
Echezona Oji <echyth@yahoo.com>
2024-02-08 22:09:19 +1000

    feature(task 7): implement script for task 7 functionality

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module when run adds all arguments to a python list
+and save them to a file
+"""
 import sys
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
