a6d97463e3c86d8c3f5b4e5838dd1a2e121ac2bb
Echezona Oji <echyth@yahoo.com>
2024-02-08 21:31:31 +1000

    feature(task 3): implement to_json_string function

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Module for function to_json_string"""
 import json
 
 
 def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)
+
+    Args:
+        my_obj (obj): object to stringify
+
+    Return:
+        str: JSON representation of my_obj
+    """
     return json.dumps(my_obj)
