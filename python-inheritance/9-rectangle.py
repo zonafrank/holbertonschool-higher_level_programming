04987a98bc0571934caaed31dc22468d8a120546
Echezona Oji <echyth@yahoo.com>
2024-02-08 20:51:16 +1000

    feature(task 9): implement __str__ method for rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""Module for the Rectangle class"""
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle class which inherits from the BaseGeometry class"""
+
     def __init__(self, width, height):
+        """instance initialization method"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """Calculates and returns the area of the rectangle instance
+        Return:
+            int: area of the rectangle
+        """
         return self.__width * self.__height
 
     def __str__(self):
+        """Returns a string which describes the rectangle"""
         return f"[Rectangle] {self.__width}/{self.__height}"
