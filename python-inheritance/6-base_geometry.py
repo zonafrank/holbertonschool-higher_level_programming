06659d8675685f2a40798521fe2e8a33f0878ebb
Echezona Oji <echyth@yahoo.com>
2024-02-08 16:14:52 +1000

    doc(task 6): add module, class and function documentation

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module for class BaseGeomtry"""
+
 
 class BaseGeometry:
+    """class BaseGeometry"""
+
     def area(self):
+        """calculates area"""
         raise Exception("area() is not implemented")
