b699dafc18222aa87a3859c8b24372e04a94e641
Echezona Oji <echyth@yahoo.com>
2024-02-08 22:17:08 +1000

    feature(task 7): implement function class_to_json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import json
+"""Module for function class_to_json"""
 
 
 def class_to_json(obj):
+    """eturns the dictionary description with simple data structure"""
     return json.dumps(obj.__dict__)
