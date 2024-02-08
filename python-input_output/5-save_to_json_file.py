960417c4dfb78fbfe39d4eb410ae65bd36786f94
Echezona Oji <echyth@yahoo.com>
2024-02-08 21:50:55 +1000

    feature(task 5): implement function save_to_json_file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Module for the function save_to_json_file"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation:
+
+    Args:
+        my_obj (obj): python object to write to file
+        filename (str): name of file to write to
+
+    Return:
+        Nothing
+    """
     with open(filename, "w") as f:
         json.dump(my_obj, f)
