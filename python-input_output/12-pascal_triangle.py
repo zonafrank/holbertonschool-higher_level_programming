c19b4c53aff98b233ea374035bde7b9445d9ec68
Echezona Oji <echyth@yahoo.com>
2024-02-08 23:11:47 +1000

    feature(task 12): implement function pascal_triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Module for function to generate pascal's triangle"""
+
 
 def pascal_triangle(n):
+    """Generate a Pascal's Triangle
+
+    Args:
+        n (int): number of layers
+
+    Returns:
+        Representation of the triangle using lists
+    """
     triangle = []
     for i in range(n):
         row = []
