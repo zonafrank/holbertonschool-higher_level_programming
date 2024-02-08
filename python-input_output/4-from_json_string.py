20655598b27f15bc2da8e0f60a02422e3b2d3b23
Echezona Oji <echyth@yahoo.com>
2024-02-08 21:36:52 +1000

    feature(task 4): implement from_json_string function

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Module for function from_json_strint"""
 import json
 
 
 def from_json_string(my_str):
+    """Converts a JSON string to a python object
+
+    Args:
+        my_str (str): the string to convert to JSON
+
+    Return:
+        obj: a python object
+    """
     return json.loads(my_str)
