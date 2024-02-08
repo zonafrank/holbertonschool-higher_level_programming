646a3ee1401494ab406022c1fbdc2b1037362b50
Echezona Oji <echyth@yahoo.com>
2024-02-08 22:34:32 +1000

    fix(task 8): modify return value

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import json
 """Module for function class_to_json"""
 
 
 def class_to_json(obj):
     """eturns the dictionary description with simple data structure"""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
