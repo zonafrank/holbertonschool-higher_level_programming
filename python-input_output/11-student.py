0b8c7cbc4df4a61f669f49c1666d2092fb6d3211
Echezona Oji <echyth@yahoo.com>
2024-02-08 23:05:45 +1000

    feature(task 11): implement reload_from_json method

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,21 +1,48 @@
 #!/usr/bin/python3
+"""Module for Student class"""
 
 
 class Student:
     def __init__(self, first_name, last_name, age):
+        """initialization method for instances
+
+        Args:
+            first_name (str): first  name
+            last_name (str): last name
+
+            Return:
+                Nothing
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if not attrs:
-            return self.__dict__
-        result = {}
-        for key in self.__dict__:
-            if key in attrs:
-                result[key] = self.__dict__[key]
-        return result
+        """gets dict of square.
+
+            Returns:
+                str: json version of class dict
+        """
+
+        dict = self.__dict__
+        filtered = {}
+        if attrs is not None:
+            for key in dict:
+                if key in attrs:
+                    filtered[key] = dict[key]
+        else:
+            filtered = dict
+
+        return filtered
 
     def reload_from_json(self, json):
+        """replaces instance attrs with json values.
+
+            Args:
+                json (dict): dict with attrs to replace
+
+            Returns:
+                str: json version of class dict
+        """
         for key in json:
             self.__dict__[key] = json[key]
