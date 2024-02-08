0f367ae437bd32c132802efca487c5bf3ee2d627
Echezona Oji <echyth@yahoo.com>
2024-02-08 21:10:11 +1000

    feature(task 0): implement function read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Module for function read_file"""
+
 
 def read_file(filename=""):
+    """reads from a file
+
+    Args:
+        filename (string): name of the file to read
+    """
     if filename:
         with open(filename, "r", encoding="utf-8") as f:
             for line in f:
