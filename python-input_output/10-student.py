74811d8537bb36a0a11772197aa67fd196266a6d
Echezona Oji <echyth@yahoo.com>
2024-02-08 22:52:09 +1000

    feature(task 10): imlement attr filtering in to_json method of Student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
+"""Module for Student class"""
 
 
 class Student:
+    """class Student"""
+
     def __init__(self, first_name, last_name, age):
+        """initialization method for instances
+
+        Args:
+            first_name (str): first  name
+            last_name (str): last name
+
+            Return:
+                Nothing
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """gets dict of square.
+
+            Args:
+                attrs (list): list of attributes
+
+            Returns:
+                obj: dict with keys matching values in attrs
+        """
         if not attrs:
             return self.__dict__
         result = {}
