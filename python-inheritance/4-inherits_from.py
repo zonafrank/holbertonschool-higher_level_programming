bad3c0391d8146d0759b5cb353696cbdef2540ab
Echezona Oji <echyth@yahoo.com>
2024-02-08 16:09:35 +1000

    doc(task 4): add module and function documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Module for function `inherits_from`"""
+
 
 def inherits_from(obj, a_class):
+    """returns true / false if obj inherits from a_class or not."""
     if (type(obj).__name__ == a_class.__name__):
         return False
     return issubclass(type(obj), a_class)
