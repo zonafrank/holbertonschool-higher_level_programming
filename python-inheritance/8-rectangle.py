bd68913ec3d32829dee024327fd80ff87e876844
Echezona Oji <echyth@yahoo.com>
2024-02-08 16:32:34 +1000

    feature(task 8): implement class Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Module for the Rectangle class"""
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle class which inherits from the BaseGeometry class"""
+
     def __init__(self, width, height):
+        """instance initialization method"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
