af10562140e6d638fbf08043929eec20f5365e11
Echezona Oji <echyth@yahoo.com>
2024-02-08 21:23:03 +1000

    feature(task 2): implement function append_write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""Module for function append_write"""
+
 
 def append_write(filename="", text=""):
+    """appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+
+    Args:
+        filename (str): name of the file to append text to
+        text (str): text to append to the file
+
+    Return:
+        int: number of characters added to filename
+    """
     if not filename:
         return
     with open(filename, "a", encoding="utf-8") as f:
