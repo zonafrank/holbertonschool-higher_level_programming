86a3e346f7705a5b461173b7d82afbf7d79f9d70
Echezona Oji <echyth@yahoo.com>
2024-02-08 21:18:48 +1000

    feature(task 1): implement function write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Module for the function write_file"""
+
 
 def write_file(filename="", text=""):
+    """ writes a string to a text file (UTF8)
+    and returns the number of characters written:
+
+    Args:
+        filename (str): name of file to write to
+
+    Return:
+        int: number of characters written to filname
+    """
     if not filename:
         return
 
