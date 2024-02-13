1d978a3a5d2d64017129f9a6be5bacc0251a3222
Echezona Oji <echyth@yahoo.com>
2024-02-13 22:14:35 +1000

    add tests for instantiating Rectangle class with no args and with two args

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,5 +8,19 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """Test functions for the Rectangle class"""
 
-    def test_always_passes(self):
-        self.assertTrue(True)
+    def test_instantiaton_without_args(self):
+        """
+        Creating a Rectangle instance with no arguments
+        fails with a TypeError exception
+        """
+        self.assertRaises(TypeError, Rectangle)
+
+    def test_instatiation_with_two_args(self):
+        """
+        Creating a Rectangle instance with two arguments
+        succeeds with width set to the first argument
+        and height set to the second argument
+        """
+        r = Rectangle(1, 2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
