4e314d109527429516d76cf003a6d6de8857ad1d
Echezona Oji <echyth@yahoo.com>
2024-02-13 21:24:48 +1000

    feature(task 0): add test test_id_autogen

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,8 +6,11 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """Test functions for base class"""
+
     def test_id_autogen(self):
         """Test auto generation of id for Base instance"""
         b = Base()
-        # id_assigned = b > 0
         self.assertTrue(b.id > 0)
+        c = Base()
+        self.assertEqual(c.id, b.id + 1)
