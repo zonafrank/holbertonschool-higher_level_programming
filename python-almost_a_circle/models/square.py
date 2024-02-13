7e86a0ab908944c24e1a44b078bbb58278b14c3d
Echezona Oji <echyth@yahoo.com>
2024-02-13 10:22:18 +1000

    feature(task 12): implement update method in square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -29,3 +29,25 @@ class Square(Rectangle):
     def size(self, val):
         self.width = val
         self.height = val
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute:"""
+        if args:
+            for idx, val in enumerate(args):
+                if idx == 0:
+                    self.id = val
+                elif idx == 1:
+                    self.width = val
+                    self.height = val
+                elif idx == 3:
+                    self.x = val
+                elif idx == 4:
+                    self.y = val
+        else:
+            if kwargs:
+                for key in kwargs:
+                    if key == "size":
+                        self.__setattr__("width", kwargs[key])
+                        self.__setattr__("height", kwargs[key])
+                    else:
+                        self.__setattr__(key, kwargs[key])
