12bc88bd14aacf9a26803fcacd61b7a2bb968613
Echezona Oji <echyth@yahoo.com>
2024-03-12 09:15:58 +1000

    feat(task 0): lists privileges of user_0d_1 and user_0d_2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all privileges of the MySQL user user_0d_1 on server (in localhost)
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists all privileges of the MySQL user user_0d_2 on server (in localhost)
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
