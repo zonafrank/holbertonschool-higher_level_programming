1cba71a01803634fa71503807a20d1341f7b6421
Echezona Oji <echyth@yahoo.com>
2024-03-11 12:16:50 +1000

    feat(task 100): convert  hbtn_0c_0 database to UTF8

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- script that converts hbtn_0c_0 database to UTF8
+-- convert database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- change collation for first_table
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- change collation for `name` field in first_table
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
