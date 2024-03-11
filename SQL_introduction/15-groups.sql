fc93c54044c97e8b2d31c34106da2a69de74d8d7
Echezona Oji <echyth@yahoo.com>
2024-03-11 10:15:42 +1000

    feat(task 15): list number of records grouped by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- script that lists the number of records with
+-- the same score in the table second_table
+-- displays the score
+-- displays the number of records for this score with the label `number`
+-- is sorted by the number of records (descending)
+SELECT score, count(*) AS number
+FROM second_table
+GROUP BY score;
