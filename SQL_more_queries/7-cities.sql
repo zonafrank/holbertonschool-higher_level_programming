d9c938841acf41a1cbeb31e8beb887ae632f4651
Echezona Oji <echyth@yahoo.com>
2024-03-12 22:17:49 +1000

    feat(task 7): create db hbtn_0d_usa and table cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,17 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+
+-- cities description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, script should not fail
+-- If the table cities already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+  id INT AUTO_INCREMENT NOT NULL,
+  state_id INT NOT NULL,
+  name VARCHAR(256),
+  PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
