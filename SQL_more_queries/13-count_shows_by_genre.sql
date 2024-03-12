1a6f232921d0eea72d63641b56e6dc1ae9281910
Echezona Oji <echyth@yahoo.com>
2024-03-12 23:55:06 +1000

    feat(task 13): list number of shows for each genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 --  script that lists all genres and displays the number of shows linked to each
-SELECT tv_genres.name as genre, COUNT(tv_shows.title) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
