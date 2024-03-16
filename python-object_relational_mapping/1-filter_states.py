45a417a327f073271ca5c2b7adef03f2b2ceb076
Echezona Oji <echyth@yahoo.com>
2024-03-17 09:33:32 +1000

    feat(task 1): add script that lists all states with a name starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa:
+
+Takes 3 arguments: username, password and database name
+No argument validation needed
+Uses the module MySQLdb (import MySQLdb)
+connects to a MySQL server running on localhost at port 3306
+Results are sorted in ascending order by states.id
+Code is not be executed when imported
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    _, username, password, db_name = sys.argv
+    host = "localhost"
+    port = 3306
+    db = MySQLdb.connect(user=username, password=password,
+                         host=host, port=port, database=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    for row in cur.fetchall():
+        print(row)
+    db.close()
