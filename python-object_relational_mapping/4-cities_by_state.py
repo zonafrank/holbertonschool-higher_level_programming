e700df48e1f8ca918c49f9cb69fc7da919293d78
Echezona Oji <echyth@yahoo.com>
2024-03-17 10:35:57 +1000

    feat(task 4): add script to list all cities from database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+a script lists all cities from the database hbtn_0e_4_usa:
+
+Takes 3 arguments: username, password and database name
+No argument validation needed
+Uses the module MySQLdb (import MySQLdb)
+connects to a MySQL server running on localhost at port 3306
+Results are sorted in ascending order by cities.id
+Code is not be executed when imported
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    _, username, password, db_name, state_name = sys.argv
+    host = "localhost"
+    port = 3306
+    db = MySQLdb.connect(user=username, password=password,
+                         host=host, port=port, database=db_name)
+    with db.cursor() as cur:
+        cur.execute(
+            """SELECT *
+            FROM cities AS A
+            JOIN states AS B ON A.state_id = B.id
+            ORDER BY A.id ASC;"""
+        )
+
+        for row in cur.fetchall():
+            print(row)
+
+    db.close()
