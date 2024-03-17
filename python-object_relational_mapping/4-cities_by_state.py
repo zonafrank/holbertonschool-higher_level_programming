45a2580e597c7385f7e9bc2b6b766dc0bad1e82f
Echezona Oji <echyth@yahoo.com>
2024-03-17 10:38:20 +1000

    feat(task 4): add script to list all cities from database

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    _, username, password, db_name, state_name = sys.argv
+    _, username, password, db_name = sys.argv
     host = "localhost"
     port = 3306
     db = MySQLdb.connect(user=username, password=password,
