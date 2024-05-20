5dbdb39ec1ad8e5530b227bff690cc0223ce0001
Echezona Oji <echyth@yahoo.com>
2024-05-20 20:55:47 +0800

    feat (task 1): initial commit

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+let red_header = document.querySelector("#red_header");
+red_header.addEventListener("click", () => {
+  let header = document.querySelector("header");
+  header.style.color = "#FF0000";
+});
