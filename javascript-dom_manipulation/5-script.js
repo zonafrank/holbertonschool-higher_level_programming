cda5e5eaa2d933ecfaa30be989c94164e8fb31dd
Echezona Oji <echyth@yahoo.com>
2024-05-20 21:26:46 +0800

    feat (task 6): initial commit

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+const updateHeader = document.querySelector("#update_header");
+const header = document.querySelector("header");
+
+updateHeader.addEventListener("click", () => {
+  header.textContent = "New Header!!!";
+});
---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,13 @@
+const url = "https://swapi-api.hbtn.io/api/people/5/?format=json";
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const character = document.querySelector("#character");
+    character.textContent = data.name;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
