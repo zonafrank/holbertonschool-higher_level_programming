38b9b7a0122607607e416dfca304620426bad5ba
Echezona Oji <echyth@yahoo.com>
2024-05-20 21:44:00 +0800

    feat (task 8): initial commit

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,11 @@
+const url = "https://hellosalut.stefanbohacek.dev/?lang=fr";
+
+function handleData(data) {
+  const helloId = document.querySelector("#hello");
+  helloId.textContent = data.hello;
+}
+
+fetch(url)
+  .then((response) => response.json())
+  .then(handleData)
+  .catch(console.error);
